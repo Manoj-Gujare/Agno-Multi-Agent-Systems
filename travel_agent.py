70d36835843ace21820bf13cd246df48e9585aad
Manoj Gujare <manojgujare726@gmail.com>
2026-02-04 21:44:53 +0530

    travel guide multi agent system

---
+++ travel_agent.py
@@ -0,0 +1,58 @@
+import os
+from dotenv import load_dotenv
+from agno.agent import Agent
+from agno.team import Team
+from agno.models.aws import AwsBedrock
+from agno.tools.duckduckgo import DuckDuckGoTools
+
+# Load environment variables from .env file
+load_dotenv()
+
+
+## 1. News search agent
+## 2. Web Search agent
+
+# define the model
+llm = AwsBedrock(id="us.anthropic.claude-3-7-sonnet-20250219-v1:0",
+                aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+                aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+                aws_region=os.getenv("AWS_REGION"))
+
+# news search agent
+news_agent = Agent(
+    id="news_agent",
+    name="News Agent",
+    model=llm,
+    role="Get the latest news headlines and give summary",
+    instructions=["You are a news search agent",
+                  "Search for the latest news for a particular destination and give a summarized view"],
+    tools=[DuckDuckGoTools()]
+)
+
+# web search agent
+web_search_agent = Agent(
+    id="web_search_agent",
+    name="Web Search Agent",
+    model=llm,
+    role="Get information for places to visit at the destination",
+    instructions=["You are a web search agent",
+                  "your goal is to search for places of interest for a particular destination"],
+    tools=[DuckDuckGoTools()]
+)
+
+# form the team
+travel_agent = Team(
+    members=[news_agent, web_search_agent],
+    model=llm,
+    name="Travel Agent",
+    id="travel_agent",
+    role="You are a team leader and you delegate tasks to members",
+    instructions=["You are an expert travel agent",
+                  "you team members can get latest news and places of interest for a particular destination",
+                  "your task is to present the output using proper headlines"],
+    stream=True,
+    markdown=True,
+    debug_mode=True
+)
+
+travel_agent.cli_app(stream=True, markdown=True)
